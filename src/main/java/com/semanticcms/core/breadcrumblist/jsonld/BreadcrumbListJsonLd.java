f673584aaba60870c6c541cc1a8cd7cb351c2c85
AO Industries, Inc <support@aoindustries.com>
2021-06-05 01:00:48 -0500

    Open-source projects moved to "com.aoapps" packages

--- src/main/java/com/semanticcms/core/breadcrumblist/jsonld/BreadcrumbListJsonLd.java
+++ src/main/java/com/semanticcms/core/breadcrumblist/jsonld/BreadcrumbListJsonLd.java
@@ -22,12 +22,12 @@
  */
 package com.semanticcms.core.breadcrumblist.jsonld;
 
-import com.aoindustries.encoding.EncodingContext;
-import static com.aoindustries.encoding.JavaScriptInXhtmlEncoder.ldJsonInXhtmlEncoder;
-import com.aoindustries.encoding.MediaWriter;
-import static com.aoindustries.encoding.TextInJavaScriptEncoder.textInLdJsonEncoder;
-import com.aoindustries.html.servlet.DocumentEE;
-import com.aoindustries.net.URIEncoder;
+import com.aoapps.encoding.EncodingContext;
+import static com.aoapps.encoding.JavaScriptInXhtmlEncoder.ldJsonInXhtmlEncoder;
+import com.aoapps.encoding.MediaWriter;
+import static com.aoapps.encoding.TextInJavaScriptEncoder.textInLdJsonEncoder;
+import com.aoapps.html.servlet.DocumentEE;
+import com.aoapps.net.URIEncoder;
 import com.semanticcms.core.model.Book;
 import com.semanticcms.core.model.Page;
 import com.semanticcms.core.model.PageRef;
